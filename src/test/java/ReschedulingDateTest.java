130913760df153ad1305c13aac8f0897e0ecd49e
Ирина Пащенко <i.m.uglanova@gmail.com>
2022-11-25 16:50:00 +0300

    report generation with successful test

--- src/test/java/ReschedulingDateTest.java
+++ src/test/java/ReschedulingDateTest.java
@@ -42,7 +42,7 @@ public class ReschedulingDateTest {
     void shouldReturnOkMessage() {
 
         UsersData user1 = DataGenerator.RequestForDelivery.generateInfo("ru");
-        String planningDate1 = DataGenerator.RequestForDelivery.generateDate(5);
+        String planningDate1 = DataGenerator.RequestForDelivery.generateDate(4);
         String planningDate2 = DataGenerator.RequestForDelivery.generateDate(6);
 
 
